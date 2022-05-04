48bc856b32801ea7460b8bc391bd75fe7394af49
Ta Ngo Quoc Quan <quanta23122@gmai.com>
2022-05-04 09:06:08 +0700

    pt1

---
+++ DEMO_INTERNET/config.php
@@ -0,0 +1,7 @@
+<?php 
+define('host', 'localhost');
+define('user', 'root');
+define('pass', '');
+define('db', 'quanlithuvien');
+
+$conn = mysqli_connect(host, user, pass, db) or die('Unable to Connect');
